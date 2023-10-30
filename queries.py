2a58de79cd1c979bf89c4e00847c226e3bba7973
Parissa <zahra.pmf@gmail.com>
2023-10-30 16:18:03 +0100

    2 functions are done only function of waiting time is remained

--- queries.py
+++ queries.py
@@ -1,16 +1,42 @@
 # pylint:disable=C0111,C0103
+import sqlite3
 
-def query_orders(db):
+conn = sqlite3.connect('data/ecommerce.sqlite')
+db = conn.cursor()
+
+def query_orders(dbb):
     # return a list of orders displaying each column
-    pass  # YOUR CODE HERE
+    query = '''
+    SELECT * FROM Orders
+    ORDER BY OrderID ASC
+    '''
+    db.execute(query)
+    results = db.fetchall()
+    return results
 
 def get_orders_range(db, date_from, date_to):
     # return a list of orders displaying all columns with OrderDate between
     # date_from and date_to (excluding date_from and including date_to)
-    pass  # YOUR CODE HERE
+    query = f'''
+    SELECT * FROM Orders
+    WHERE OrderDate > "{date_from}" AND OrderDate <= "{date_to}"
+    ORDER  BY OrderDate ASC
+    '''
+    db.execute(query)
+    results = db.fetchall()
+    return results
 
 def get_waiting_time(db):
     # get a list with all the orders displaying each column
     # and calculate an extra TimeDelta column displaying the number of days
     # between OrderDate and ShippedDate, ordered by ascending TimeDelta
-    pass  # YOUR CODE HERE
+    query = '''
+
+    '''
+    db.execute(query)
+    results = db.fetchall()
+    return results
+
+
+#print(query_orders (db))
+#print(get_orders_range (db, 2012-4-28, 2012-9-13))
